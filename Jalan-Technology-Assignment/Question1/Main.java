4a2108e73f9fabef18b3e41cc43f232926c55bc5
Rakesh Kumar <acousticrakesh0827@gmail.com>
2023-01-14 18:00:49 +0530

    Omeducom Solution Project Commit

--- Jalan-Technology-Assignment/Question1/Main.java
+++ Jalan-Technology-Assignment/Question1/Main.java
@@ -1,35 +1,12 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
         int [] arr = {-4, 1, 3, -2, -1};
-        findTerms4(arr);
-
+        findTerms(arr);
     }
 
-
-    // This is the second approach to fint the all the terms whose sum is zero :
-    // This is quite optimized than the above one :
-    public static int[] findTerms2(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n-2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                       return  new int[]{arr[i], arr[j], arr[k]};
-                    }
-                }
-            }
-        }
-        return new int[]{0};
-
-    }
-
-
-
-
-    public static void findTerms4(int[] arr) {
+    public static void findTerms(int[] arr) {
         Arrays.sort(arr);
         int n = arr.length;
         boolean found = false;
